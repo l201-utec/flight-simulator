877be874e3faf85b1a4ebdf570cb55b78a67729b
fgarciacardenas <facundoxgc@gmail.com>
2020-01-23 17:08:47 -0500

    The ability to switch player perspective was added

---
+++ Assets/Scripts/Perspective/SwitchPerspective.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchPerspective : MonoBehaviour
+{
+    public Camera cam1;
+    public Camera cam2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam1.enabled = true;
+        cam2.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C)) {
+         cam1.enabled = !cam1.enabled;
+         cam2.enabled = !cam2.enabled;
+        }
+    }
+}
