5e2a7cb638b78e80a5fa9672512015f293ae9f33
Nelsonxxji <nelsoneduardoandrest@gmail.com>
2020-01-29 16:37:44 -0500

    Partial Fix #20-1

---
+++ Assets/Scripts/Pivot/Gizmo.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gizmo : MonoBehaviour
+{
+    public float gizmoSize = .75f;
+    public Color gizmoColor = Color.yellow;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = gizmoColor;
+        Gizmos.DrawWireSphere(transform.position, gizmoSize);
+    }
+}
