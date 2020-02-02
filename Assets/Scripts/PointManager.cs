37883b5dbb060ba5e9ce7b0d59de3ad98a9305a0
Diego Cánez <canezdiego@gmail.com>
2020-02-02 16:41:34 -0500

    Added Point Manager base

---
+++ Assets/Scripts/PointManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public int spaceBetweenPoints = 10;
+    public int lenght_x;
+    public int lenght_y;
+    public int lenght_z;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
