bc221f2faac9b2bb872453ff086e17c4630478d7
fgarciacardenas <facundoxgc@gmail.com>
2020-02-01 19:26:33 -0500

    Better colliders

---
+++ Assets/DamageDetection.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDetection : MonoBehaviour
+{
+    public GameObject smoke;
+    public GameObject explosion;
+    public int collisionCount = 0;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            var colObj = contact.thisCollider;
+            GameObject planeSmoke = Instantiate(smoke, colObj.transform.position, colObj.transform.rotation);
+            planeSmoke.transform.parent = transform;
+            collisionCount++;
+
+            Debug.LogWarning($"Counts: {collisionCount}");
+            if (collisionCount >= 3)
+            {
+                Instantiate(explosion, colObj.transform.position, colObj.transform.rotation);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
